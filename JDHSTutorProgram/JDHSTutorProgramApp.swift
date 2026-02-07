f6590195fb3c1e6f82e8c2d643e95e12cf2c996e
Paul Casiero <paulie@casiero.com>
2026-02-07 17:12:07 -0500

    Initial Commit

---
+++ JDHSTutorProgram/JDHSTutorProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  JDHSTutorProgramApp.swift
+//  JDHSTutorProgram
+//
+//  Created by Paul Casiero on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct JDHSTutorProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
